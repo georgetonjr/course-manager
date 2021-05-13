b74a14dd09455331b22e962fbd8f206fc6b4a408
georgetonjr <37754623+georgetonjr@users.noreply.github.com>
2021-05-13 15:58:02 -0300

    add info page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { FormsModule } from "@angular/forms";
 import { BrowserModule } from "@angular/platform-browser";
 import { RouterModule } from "@angular/router";
 import { AppComponent } from "./app.component";
+import { CourseInfoComponent } from "./courses/course-info.component";
 import { CourseListComponent } from "./courses/course-list.component";
 import { Error404Component } from "./error-404/error-404.component";
 import { NavBarComponent } from "./nav-bar/nav-bar.component";
@@ -17,19 +18,24 @@ import { StarComponent } from "./star/star.component";
     ReplacePipe,
     NavBarComponent,
     Error404Component,
+    CourseInfoComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
     RouterModule.forRoot([
       {
+        path: "courses",
+        component: CourseListComponent,
+      },
+      {
+        path: "courses/info/:id",
+        component: CourseInfoComponent,
+      },
+      {
         path: "",
         redirectTo: "courses",
         pathMatch: "full",
-      },
-      {
-        path: "courses",
-        component: CourseListComponent,
       },
       {
         path: "**",
